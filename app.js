eef1ff0f3a39f6e6cfd094d25ba54b7954e98a61
“Nix” <xzm3713@gmail.com>
2016-08-26 16:55:57 +0800

    change pwd

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ router.post('/signin', async (ctx, next) => {
         name = ctx.request.body.name || '',
         password = ctx.request.body.password || '';
     console.log(`signin with name: ${name}, password: ${password}`);
-    if (name === 'koa' && password === '12345') {
+    if (name === 'koa' && password === 'koa') {
         ctx.response.body = `<h1>Welcome, ${name}!</h1>`;
     } else {
         ctx.response.body = `<h1>Login failed!</h1>
